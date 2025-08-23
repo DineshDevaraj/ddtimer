9aec0791463a7940e55fae7b57d414135b64675f
dinesh.devaraj <ddinesh.swdev@gmail.com>
2025-08-23 18:39:26 +0530

    function to delete message-pallet

--- static/right_panel/main.js
+++ static/right_panel/main.js
@@ -1,8 +1,14 @@
 
 function addNewMessage(event) {
     const message = MessagePallet.clone();
+    message.dom.querySelector('textarea').value = '';
+    message.dom.querySelector('[name=delete]').onclick = deleteMessage;
     const palletContainer = document.querySelector('.right-panel .pallet-container');
     palletContainer.appendChild(message.dom);
+}
+
+function deleteMessage(event) {
+    event.target.closest('.message-pallet').remove();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
