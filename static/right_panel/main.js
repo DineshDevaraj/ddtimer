f0a0afc54f5d01ec0e7ae7d3fc281d07540436b5
dinesh.devaraj <ddinesh.swdev@gmail.com>
2025-08-24 07:17:00 +0530

    hide message button and function

--- static/right_panel/main.js
+++ static/right_panel/main.js
@@ -2,10 +2,17 @@
 function addNewMessage(event) {
     const message = MessagePallet.clone();
     message.dom.querySelector('textarea').value = '';
-    message.dom.querySelector('[name=show]').onclick = showMessage;
-    message.dom.querySelector('[name=delete]').onclick = deleteMessage;
+    message.dom.querySelector('[name=hide]').classList.add('d-none');
+    message.dom.querySelector('[name=show]').classList.remove('d-none');
     const palletContainer = document.querySelector('.right-panel .pallet-container');
     palletContainer.appendChild(message.dom);
+    setEventHandlers(message);
+}
+
+function setEventHandlers(messagePallet) {
+    messagePallet.dom.querySelector('[name=show]').onclick = showMessage;
+    messagePallet.dom.querySelector('[name=hide]').onclick = hideMessage;
+    messagePallet.dom.querySelector('[name=delete]').onclick = deleteMessage;
 }
 
 function deleteMessage(event) {
@@ -15,13 +22,21 @@ function deleteMessage(event) {
 function showMessage(event) {
     const messagePallet = event.target.closest('.message-pallet');
     const message = messagePallet.querySelector('textarea').value;
+    messagePallet.querySelector('[name=hide]').classList.remove('d-none');
+    messagePallet.querySelector('[name=show]').classList.add('d-none');
     TimerCard.showMessage(message);
+}
+
+function hideMessage(event) {
+    const messagePallet = event.target.closest('.message-pallet');
+    messagePallet.querySelector('[name=show]').classList.remove('d-none');
+    messagePallet.querySelector('[name=hide]').classList.add('d-none');
+    TimerCard.hideMessage();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     const messagePallet = MessagePallet.getByMessageId("message-1");
-    messagePallet.dom.querySelector('[name=show]').onclick = showMessage;
-    messagePallet.dom.querySelector('[name=delete]').onclick = deleteMessage;
     const addNewMessageButton = document.getElementById('add-new-message');
     addNewMessageButton.onclick = addNewMessage;
+    setEventHandlers(messagePallet);
 });
