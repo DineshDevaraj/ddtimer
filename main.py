b7b1c3d123565c16573f09b29b6b2aef01e3925b
dinesh.devaraj <ddinesh.swdev@gmail.com>
2025-08-27 09:39:57 +0530

    add timer prefix before urk

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+router = APIRouter()
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(router, prefix="/timer")
 
-@app.get("/")
+
+@router.get("/")
 def read_root():
     return templates.TemplateResponse("index.html", {"request": {}})
+
 
 # uvicorn main:app --reload --host 0.0.0.0 --port 7070
